3129b1f6b61ad8d0b315ece644f5a34bb7f74be2
David Valachovic <David.E.Valachovic@gmail.com>
2022-01-23 14:56:50 -0600

    wiggly red square 🟥

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,30 @@
 import JSX from './custom-render'
 import './app.css'
+import { vec } from './excalibur/engine'
+
+const canvasSize = vec(640, 480)
+
+const canvas = (
+  <canvas id="glCanvas" width={canvasSize.x} height={canvasSize.y} />
+) as HTMLCanvasElement
 
 document.body.appendChild(<h1>howdy world 🤠</h1>)
+
+document.body.appendChild(canvas)
+
+const ctx = canvas.getContext('2d')
+
+ctx.fillStyle = 'red'
+
+// Door
+ctx.fillRect(110, 190, 40, 60);
+
+function loop() {
+  ctx.clearRect(0, 0, canvasSize.x, canvasSize.y);
+  const x = (Math.sin(Date.now() / 100) * 50) + 100
+  console.log(x)
+  ctx.fillRect(x, 190, 40, 60);
+  requestAnimationFrame(loop)
+}
+
+requestAnimationFrame(loop)
